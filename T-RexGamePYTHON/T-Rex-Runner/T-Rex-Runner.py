e5afe1e3ef35501b19a7187aca67eae7fe064432
UmeshV1999 <umeshverma1999.uv@gmail.com>
2019-07-11 02:43:08 +0530

    Initial commit

---
+++ T-RexGamePYTHON/T-Rex-Runner/T-Rex-Runner.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+display_width=800
+display_height=600
+black=(0,0,0)
+white=(255,255,255)
+red=(255,0,0)
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('Dinosaur runner')
+clock=pygame.time.Clock()
+dinoImg=pygame.image.load()
+pygame.quit()
+quit()
